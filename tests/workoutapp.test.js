034059cbd60ff045f44f7aa698ead2838ed293e4
workouttoken <134047372+workouttoken@users.noreply.github.com>
2023-08-18 16:23:42 +0200

    Update workoutapp.test.js

--- tests/workoutapp.test.js
+++ tests/workoutapp.test.js
@@ -2,12 +2,11 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-
 it("initial deploy", async function () {
-  const WT = await ethers.getContractFactory("WorkoutToken");
-  const wt = await WT.deploy();
-  await wt.deployed();
-  console.log('WorkoutToken deployed at:'+ wt.address);
+    const WT = await ethers.getContractFactory("WorkoutToken");
+    const wt = await WT.deploy();
+    await wt.deployed();
+    console.log('WorkoutToken deployed at:'+ wt.address);
 });
 
 it("start distribution - should returns 'Caller is not super owner' information", async function () {
@@ -44,7 +43,6 @@ it('transfer test', async () => {
     expect(newBalance.toNumber()).to.be.equal(initialBalance.toNumber() + amount);
 });
 
-
 it('should allow spender to transfer tokens on behalf of owner', async () => {
     const WT = await ethers.getContractFactory("WorkoutToken");
     const wt = await WT.deploy();
@@ -65,4 +63,76 @@ it('should allow spender to transfer tokens on behalf of owner', async () => {
     // Check recipient's balance
     const recipientBalance = await wt.balanceOf(recipient.address);
     expect(recipientBalance.toNumber()).to.equal(amount, 'Incorrect recipient balance after transferFrom');
+});
+
+it("add new owner address (should be reverted - not super owner)", async () => {
+    const WT = await ethers.getContractFactory("WorkoutToken");
+    const wt = await WT.deploy();
+    await wt.deployed();
+
+    const newOwner = '0x1234567890123456789012345678901234567890'; // Example address
+
+    // Add the new owner address
+    await expect(wt.addAddress(newOwner)).to.be.reverted;
+});
+
+it("delete owner address (should be reverted - not super owner)", async () => {
+    const WT = await ethers.getContractFactory("WorkoutToken");
+    const wt = await WT.deploy();
+    await wt.deployed();
+
+    const owner = '0x1234567890123456789012345678901234567890'; // Example address
+
+    // delete owner address
+    await expect(wt.deleteAddress(owner)).to.be.reverted;
+});
+
+it("checks if address is owner", async () => {
+    const WT = await ethers.getContractFactory("WorkoutToken");
+    const wt = await WT.deploy();
+    await wt.deployed();
+
+    const owner = '0xa60a4fe0591017233Ab3b3B7de028Db23Fa48300';
+    const notOwner = '0x1234567890123456789012345678901234567890';
+
+    expect(await wt.checkIsOwner(owner)).to.be.equal(true);
+    expect(await wt.checkIsOwner(notOwner)).to.be.equal(false);
+});
+
+it("checks if caller is super owner", async () => {
+    const WT = await ethers.getContractFactory("WorkoutToken");
+    const wt = await WT.deploy();
+    await wt.deployed();
+
+    expect(await wt.checkSuperOwner()).to.be.equal(false);
+});
+
+it("checks confirmation data", async () => {
+    const WT = await ethers.getContractFactory("WorkoutToken");
+    const wt = await WT.deploy();
+    await wt.deployed();
+
+    expect(await wt.getWaitingConfirmationsList()).to.be.equal('');
+});
+
+it("addRole (should be reverted)", async () => {
+    const WT = await ethers.getContractFactory("WorkoutToken");
+    const wt = await WT.deploy();
+    await wt.deployed();
+
+    const owner = '0x1234567890123456789012345678901234567890'; // Example address
+
+    // delete owner address
+    await expect(wt.addRole('ROLE', owner)).to.be.reverted;
+});
+
+it("deleteRole (should be reverted)", async () => {
+    const WT = await ethers.getContractFactory("WorkoutToken");
+    const wt = await WT.deploy();
+    await wt.deployed();
+
+    const owner = '0x1234567890123456789012345678901234567890'; // Example address
+
+    // delete owner address
+    await expect(wt.deleteRole('ROLE', owner)).to.be.reverted;
 });
